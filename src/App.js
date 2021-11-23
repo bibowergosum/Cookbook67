c75010e19849aae46977be46c3cc7a5d39cfc35e
JoniJo06 <jonasjuergens6@gmail.com>
2021-11-23 10:46:31 +0100

    add react-router-dom@6

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 /** @format */
 
+import axios from 'axios';
+
 import './index.css';
 
 function App() {
+	axios
+		.get('https://cdn.contentful.com/spaces/5xjh8nhjizdc/entries?access_token=PCho7WhGHIZgE_Wt2AjZnhLm7HVDHvF8pm9MmYnjdHM')
+		.then((res) => console.log(res.data.items[0].fields.ingredients.content[0].content[0].value));
 	return <div className='App'></div>;
 }
 
