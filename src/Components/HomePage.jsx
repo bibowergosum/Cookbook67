05af17eb5f80483d4393cf9ea9936b35a34ef718
JoniJo06 <jonasjuergens6@gmail.com>
2021-11-25 16:24:44 +0100

    remove temp styling

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,10 +1,11 @@
 /** @format */
 import RecipeCard from './RecipeCard';
 import { useState, useEffect } from 'react';
-import { client } from './client';
+import client from './client';
 import { Grid, Box, Container } from '@mui/material';
 import Header from './Header';
 import Footer from './Footer';
+// import axios from 'axios';
 
 import './../index.css';
 
@@ -12,40 +13,46 @@ const HomePage = () => {
 	const [recipes, setRecipes] = useState([]);
 
 	useEffect(() => {
+		// axios
+		// 	.get(`https://cdn.contentful.com/spaces/${process.env.REACT_APP_SPACE_ID}/entries?access_token=${process.env.REACT_APP_ACCESS_TOKEN}`)
+		// 	.then((res) => console.log(res.data))
+		// 	.catch((err) => console.error(err));
 		client
 			.getEntries()
 			.then((res) => setRecipes(res.items))
 			.catch((err) => console.error(err));
 	}, []);
 	return (
-		<Container maxWidth='100%'>
-			<header>
+		<>
+			<header style={{ width: '100vw' }}>
 				<Header />
 			</header>
-			<main>
-				<Box sx={{ flexGrow: '1', margin: '2em 0' }}>
-					<Grid container spacing={5} justifyContent='center'>
-						{recipes.map((recipe, i) => {
-							return (
-								<Grid key={i} item xs={10} sm={6} md={4} lg={3} xl={2}>
-									<RecipeCard recipe={recipe} />
-								</Grid>
-							);
-						})}
-						{recipes.map((recipe, i) => {
-							return (
-								<Grid key={i} item xs={10} sm={6} md={4} lg={3} xl={2}>
-									<RecipeCard recipe={recipe} />
-								</Grid>
-							);
-						})}
-					</Grid>
-				</Box>
-			</main>
+			<Container maxWidth='100%'>
+				<main>
+					<Box sx={{ flexGrow: '1', margin: '2em 0' }}>
+						<Grid container spacing={5} justifyContent='center'>
+							{recipes.map((recipe, i) => {
+								return (
+									<Grid key={i} item xs={10} sm={6} md={4} lg={3} xl={2}>
+										<RecipeCard recipe={recipe} />
+									</Grid>
+								);
+							})}
+							{recipes.map((recipe, i) => {
+								return (
+									<Grid key={i} item xs={10} sm={6} md={4} lg={3} xl={2}>
+										<RecipeCard recipe={recipe} />
+									</Grid>
+								);
+							})}
+						</Grid>
+					</Box>
+				</main>
+			</Container>
 			<footer>
 				<Footer />
 			</footer>
-		</Container>
+		</>
 	);
 };
 
