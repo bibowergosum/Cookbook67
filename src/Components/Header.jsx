74c1b2f83a8b2f4c6e8c38a327661bc6f70a31bb
JoniJo06 <jonasjuergens6@gmail.com>
2021-11-29 16:25:15 +0100

    set Container maxWidth

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,15 @@
 /** @format */
 
 import './../index.css';
-import { styled } from "@mui/system";
-import { Container } from "@mui/material";
+import { styled } from '@mui/system';
+import { Container } from '@mui/material';
 
 const Header = () => {
 	return (
-    <Container id="header">
-      <h1 id="headline">Kochbuch der Extravaganz</h1>
-    </Container>
-  );
+		<Container maxWidth id='header'>
+			<h1 id='headline'>Kochbuch der Extravaganz</h1>
+		</Container>
+	);
 };
 
 export default Header;
