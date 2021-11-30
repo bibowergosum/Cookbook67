6b88fa31b6480bf3f1ac39e0c900c715f30e4c23
555ekstase <martinbibow@gmx.de>
2021-11-30 16:06:56 +0100

    fvg

--- src/Components/RecipeCard.jsx
+++ src/Components/RecipeCard.jsx
@@ -1,27 +1,35 @@
 /** @format */
 
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function MultiActionAreaCard({ recipe }) {
-	console.log(recipe);
-	return (
-		<Card sx={{ boxShadow: 'none' }}>
-			<Link to={'recipe/' + recipe.sys.id} style={{ textDecoration: 'none', color: 'black' }}>
-				<CardActionArea>
-					<CardMedia component='img' height='160' image={recipe.fields.homePicture} alt={recipe.fields.homePictureAltText} />
-					<CardContent>
-						<Typography gutterBottom variant='h5' component='div'>
-							{recipe.fields.title}
-						</Typography>
-					</CardContent>
-				</CardActionArea>
-			</Link>
-		</Card>
-	);
+  console.log(recipe);
+  return (
+    <Card sx={{ boxShadow: "none" }}>
+      <Link
+        to={"recipe/" + recipe.sys.id}
+        style={{ textDecoration: "none", color: "black", textAlign: "center" }}
+      >
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="160"
+            image={recipe.fields.homePicture}
+            alt={recipe.fields.homePictureAltText}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {recipe.fields.title}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Link>
+    </Card>
+  );
 }
